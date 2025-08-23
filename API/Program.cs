03f7df9cf930ca21952fb336f427f3a8cdc991ba
ishan <ishan@ISHANDEVICE>
2025-08-23 16:13:23 +1000

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(policy =>
+    policy.AllowAnyHeader()
+          .AllowAnyMethod()
+          .WithOrigins("http://localhost:3000","https://localhost:3000") // Adjust the origin as needed
+);
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
